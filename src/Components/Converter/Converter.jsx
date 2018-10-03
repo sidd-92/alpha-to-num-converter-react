9c9b64d4fb27ea7ae6fd669e348918b588555950
Sid <sidddharthvn2014@gmail.com>
2018-10-02 20:29:18 -0700

    New function to add up numbers and Display

--- src/Components/Converter/Converter.jsx
+++ src/Components/Converter/Converter.jsx
@@ -66,32 +66,32 @@ class Converter extends React.Component {
 
   convertToNumber(text) {
     let alphaNum = {
-      a: "0",
-      b: "1",
-      c: "2",
-      d: "3",
-      e: "4",
-      f: "5",
-      g: "6",
-      h: "7",
-      i: "8",
-      j: "9",
-      k: "10",
-      l: "11",
-      m: "12",
-      n: "13",
-      o: "14",
-      p: "15",
-      q: "16",
-      r: "17",
-      s: "18",
-      t: "19",
-      u: "20",
-      v: "21",
-      w: "22",
-      x: "23",
-      y: "24",
-      z: "25"
+      a: "1",
+      b: "2",
+      c: "3",
+      d: "4",
+      e: "5",
+      f: "6",
+      g: "7",
+      h: "8",
+      i: "9",
+      j: "10",
+      k: "11",
+      l: "12",
+      m: "13",
+      n: "14",
+      o: "15",
+      p: "16",
+      q: "17",
+      r: "18",
+      s: "19",
+      t: "20",
+      u: "21",
+      v: "22",
+      w: "23",
+      x: "24",
+      y: "25",
+      z: "26"
     };
 
     let splitText = text.split("");
@@ -103,6 +103,10 @@ class Converter extends React.Component {
     }
     return numString;
   }
+
+  addAllHandler = () => {
+    console.log("Add All");
+  };
 
   render() {
     return (
@@ -171,7 +175,8 @@ class Lists extends React.Component {
             <Button
               variant="contained"
               color="secondary"
-              style={{ marginTop: 10 }}>
+              style={{ marginTop: 10 }}
+              onClick={this.addAllHandler}>
               Add All Numbers
               <AddIcon style={{ marginLeft: 10 }} />
             </Button>
